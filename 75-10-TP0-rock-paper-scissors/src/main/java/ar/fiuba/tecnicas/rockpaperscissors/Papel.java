b12e9c99f2ce9ffca7ff5ac1b96c16c90e0963fa
nanigalli <nani16.galli@gmail.com>
2015-03-16 11:35:21 -0300

    La clase Papel esta terminada y sus test corren sin error

--- 75-10-TP0-rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Papel.java
+++ 75-10-TP0-rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Papel.java
@@ -5,4 +5,16 @@ public class Papel {
 	public Papel(){
 	}
 	
+	public Papel vs(Piedra contrincante){
+		return this;
+	}
+	
+	public Papel vs(Papel contrincante){
+		return this;
+	}
+	
+	public Tijera vs(Tijera contrincante){
+		return contrincante;
+	}
+	
 }
