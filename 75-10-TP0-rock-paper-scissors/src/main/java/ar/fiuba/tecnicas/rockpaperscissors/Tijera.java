cf8041b62f975c20ae88aa2fad4706656f836f6e
nanigalli <nani16.galli@gmail.com>
2015-03-16 11:37:44 -0300

    La clase Tijera esta terminada y corren sus tests. Ya corren todos los tests sin error.

--- 75-10-TP0-rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Tijera.java
+++ 75-10-TP0-rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Tijera.java
@@ -4,5 +4,17 @@ public class Tijera {
 	
 	public Tijera(){
 	}
+	
+	public Piedra vs(Piedra contrincante){
+		return contrincante;
+	}
+	
+	public Tijera vs(Tijera contrincante){
+		return this;
+	}
+	
+	public Tijera vs(Papel contrincante){
+		return this;
+	}
 
 }
